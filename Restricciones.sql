a9a76c2eda58245425235f1731ca28ed801100e6
Bruno Ferri <brunoferri_@outlook.com>
2024-07-21 20:43:25 -0300

    restricciones.sql actualizado con un paso a paso

--- Restricciones.sql
+++ Restricciones.sql
@@ -1,1 +1,6 @@
 select * from animerank
+
+-- agregar restricciones o constrainst
+-- manejo de errores para mostrar algun mensaje que marque el error y ayude a corregirlo
+-- crear tabla de registro de errores (trigger)
+-- crear tabla de auditoria: ingreso, actualizacion y eliminacion de datos (trigger)
